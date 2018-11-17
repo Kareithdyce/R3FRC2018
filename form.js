9324d1613996e4cfb587e84afa97924f09b66eb3
kareithdyce <Kareith.dyce@gmail.com>
2018-11-16 19:41:11 -0600

    added JSON

--- form.js
+++ form.js
@@ -1,10 +1,8 @@
 function myFunction(){
-    var name = document.getElementById("name").value;
-    var company = document.getElementById("text1").value;
-    var years = document.getElementById("text2").value;
-    var goals = document.getElementById("text3").value;
-    console.log(name + "\n"+
-                company + "\n"+
-                years + "\n"+
-                goals);
+    var id  = { name: document.getElementById("name").value,
+                company: document.getElementById("text1").value,
+                years: document.getElementById("text2").value,
+                city: document.getElementById("text3").value };
+    
+    console.log(id);
             }
