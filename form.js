f443589b7236a2411fe402391fee20c2326390c6
kareithdyce <Kareith.dyce@gmail.com>
2018-11-16 18:50:13 -0600

    Added textbox reading

---
+++ form.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    var name = document.getElementById("name").value;
+    var company = document.getElementById("text1").value;
+    var years = document.getElementById("text2").value;
+    var goals = document.getElementById("text3").value;
+    console.log(name + "\n"+
+                company + "\n"+
+                years + "\n"+
+                goals);
+            }
